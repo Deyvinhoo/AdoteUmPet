31362baa632b1934296f99bcb6f19c8efa965b23
Deyvison Vianna <99549533+Deyvinhoo@users.noreply.github.com>
2022-06-04 04:29:58 -0300

    Adicionando a lista e estilizando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Titulo from '../ui/components/Titulo/Titulo';
+import Lista from '../ui/components/Lista/Lista';
 
 const Home: NextPage = () => {
   return (
@@ -12,8 +13,26 @@ const Home: NextPage = () => {
             pode <strong>adotar um pet virtualmente</strong>
           </span>
         }  />    
+
+        <Lista 
+          pets={[
+            {
+              id: 1,
+              nome: 'Bidu',
+              historia: 'icnc  oiefnwoi faowinfeo if aoiwf  iiofejoi   foij  iojfojfawfjwaopei',
+              foto: 'https://www.fazfacil.com.br/wp-content/uploads/2013/03/puppy.jpg'
+            },
+            {
+              id: 2,
+              nome: 'Coadu',
+              historia: 'fao oij foaij oi fiomn oinao  iomag o',
+              foto: 'https://www.petz.com.br/blog/wp-content/uploads/2021/11/o-que-fazer-para-o-cachorro-parar-de-lamber-final-1280x720.jpg'
+            },
+          ]}
+        />
     </div>
   )
 }
+
 
 export default Home
